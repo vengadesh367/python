837a0439ae9db156d298f5d91d17b8ce73417faf
vengadesh367 <vengadesh367@gmail.com>
2024-09-18 10:58:49 +0530

    python2.0

---
+++ second.py
@@ -0,0 +1,17 @@
+a=int(input("Input of a : "))
+
+b=int(input("Input of b: "))
+
+print("Add",a+b)
+
+print("sub",a-b)
+
+print("mul",a*b)
+
+print("div",a/b) #But it will define as float.
+
+print("div %0.2f"% (a/b))  # It will be return in round method.
+
+print("div as int",a//b)
+
+print("modulo",(a%b)) # If it's posiible to do solve it will give result (or) it will give same value as modulo you have entered.
